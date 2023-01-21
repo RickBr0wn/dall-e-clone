e2394c006b926d39bd2f8010c436606ef793937e
Rick Brown <ricky.brown.00@gmail.com>
2023-01-21 12:35:02 +0000

    first commit

---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,5 @@
+import { useState, useEffect } from 'react'
+
+export default function FormField() {
+  return <h1>FormField</h1>
+}
---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,5 @@
+import { useState, useEffect } from 'react'
+
+export default function CreatePost() {
+  return <h1>Create Post</h1>
+}
